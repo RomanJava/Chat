99212fe9899f3e4887fbb99d2b058a26c290ccfc
Roman <romanunicum@gmail.com>
2017-08-13 18:20:43 +0300

    Созданы классы NetWork

--- NetWork/src/Connection.java
+++ NetWork/src/Connection.java
@@ -1,5 +1,18 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.net.Socket;
+
 /**
  * Created by Администратор on 13.08.2017.
  */
 public class Connection {
+    private Socket socket;
+    private BufferedInputStream in;
+    private BufferedOutputStream out;
+
+
+
+
+//            new BufferedInputStream(socket.getInputStream());
+
 }
